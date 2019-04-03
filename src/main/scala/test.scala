28b1865be033eff72b1adef4d0792a782cb6adea
王德昭 <455708864@qq.com>
2019-04-03 20:38:55 +0800

    add mnist data

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -12,9 +12,9 @@ object LinearRegression{
   def main(args: Array[String]): Unit ={
     val data = sc.textFile("hdfs:///data/lr.txt")
     val parsedData = data.map{line=>
-      val parts = line.split('|')
-      LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(',').map(_.toDouble)))
-    }.cache()
+    val parts = line.split('|')
+    LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(',').map(_.toDouble)))
+  }.cache()
 
     val model = LinearRegressionWithSGD.train(parsedData,2,0.1)
 
